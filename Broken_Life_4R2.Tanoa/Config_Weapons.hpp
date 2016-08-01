866a57bc3a8cb859e9da57c841741f3482e6f555
Ryan "Ace <sbrons01@yahoo.com>
2016-07-31 23:25:16 -0400

    adding weapons

--- Broken_Life_4R2.Tanoa/Config_Weapons.hpp
+++ Broken_Life_4R2.Tanoa/Config_Weapons.hpp
@@ -38,23 +38,73 @@ class WeaponShops {
         };
     };
 
+    class hunting {
+        name = "Hunting Shop";
+        side = "civ";
+        license = "hunting";
+        level = { "", "", -1, "" };
+        items[] = {
+            { "srifle_DMR_01_F", "", 90000, -1 },
+            { "srifle_DMR_06_olive_F", "", 100000, -1 }
+        };
+        mags[] = {
+            { "10Rnd_762x51_Mag", "", 150 },
+            { "20Rnd_762x51_Mag", "", 200 }
+        };
+        accs[] = {
+            { "optic_DMS", "", 1000 }
+        }
+    };
+
+    class federal {
+        name = "Federal Firearms Shop";
+        side = "civ";
+        license = "federal";
+        level = { "", "", -1, "" };
+        items[] = {
+            { "SMG_02_F", "", 50000, -1 },
+            { "SMG_05_F", "", 55000, -1 },
+            { "SMG_01_F", "", 60000, -1 },
+            { "arifle_Mk20_plain_F", "", 150000, -1 },
+            { "arifle_TRG20_F", "", 150000, -1 },
+            { "arifle_AKS_F", "", 200000, -1 },
+            { "arifle_SPAR_01_snd_F", "", 250000, -1 }
+        };
+        mags[] = {
+            { "30Rnd_9x21_Mag", "", 1000 },
+            { "30Rnd_9x21_Mag_SMG_02", "", 1000 },
+            { "30Rnd_45ACP_Mag_SMG_01", "", 1000 },
+            { "30Rnd_545x39_Mag_F", "", 1500 },
+            { "30Rnd_556x45_Stanag", "", 2000 }
+        };
+        accs[] = {
+            { "optic_Aco", "", 40000 },
+            { "optic_Aco_grn", "", 40000 },
+            { "optic_Holosight", "", 45000 }
+        }
+    };
+
     class rebel {
-        name = "Mohammed's Jihadi Shop";
+        name = "Syndicat Weapons Suplier";
         side = "civ";
         license = "rebel";
         level[] = { "", "", -1, "" };
         items[] = {
-            { "arifle_TRG20_F", "", 25000, 2500 },
-            { "arifle_Katiba_F", "", 30000, 5000 },
-            { "srifle_DMR_01_F", "", 50000, -1 },
-            { "arifle_SDAR_F", "", 20000, 7500 }
+            { "arifle_AKM_F", "", 75000, -1 },
+            { "arifle_AKS_F", "", 75000, -1 },
+            { "arifle_TRG21_F", "", 80000, -1 },
+            { "arifle_Katiba_F", "", 90000, -1 },
+            { "arifle_Katiba_C_F", "", 90000, -1 },
+            { "srifle_DMR_01_F", "", 85000, -1 },
+            { "srifle_DMR_03_woodland_F", "", 95000, -1 }
         };
         mags[] = {
-            { "30Rnd_556x45_Stanag", "", 300 },
+            { "30Rnd_762x39_Mag_F", "", 100 },
+            { "30Rnd_545x39_Mag_F", "", 100 },
+            { "30Rnd_556x45_Stanag", "", 275 },
             { "30Rnd_65x39_caseless_green", "", 275 },
-            { "10Rnd_762x54_Mag", "", 500 },
-            { "20Rnd_556x45_UW_mag", "", 125 }
-        };
+            { "10Rnd_762x51_Mag", "", 300 },
+            { "20Rnd_762x51_Mag", "", 350 },
         accs[] = {
             { "optic_ACO_grn", "", 3500 },
             { "optic_Holosight", "", 3600 },
@@ -69,16 +119,18 @@ class WeaponShops {
         license = "";
         level[] = { "", "", -1, "" };
         items[] = {
-            { "hgun_Rook40_F", "", 1500, 500 },
-            { "hgun_Pistol_heavy_02_F", "", 2500, -1 },
+            { "hgun_Rook40_F", "", 3000, 500 },
+            { "hgun_Pistol_heavy_02_F", "", 5000, -1 },
             { "hgun_ACPC2_F", "", 4500, -1 },
-            { "hgun_PDW2000_F", "", 9500, -1 }
+            { "hgun_PDW2000_F", "", 9500, -1 },
+            { "arifle_AKM_F", "", 350000, -1 }
         };
         mags[] = {
             { "16Rnd_9x21_Mag", "", 25 },
             { "6Rnd_45ACP_Cylinder", "", 50 },
             { "9Rnd_45ACP_Mag", "", 45 },
-            { "30Rnd_9x21_Mag", "", 75 }
+            { "30Rnd_9x21_Mag", "", 75 },
+            { "30Rnd_762x39_Mag_F", "", 500 },
         };
         accs[] = {
             { "optic_ACO_grn_smg", "", 950 }
@@ -132,15 +184,12 @@ class WeaponShops {
 
     //Cop Shops
     class cop_basic {
-        name = "Altis Cop Shop";
+        name = "TSP Basic Shop";
         side = "cop";
         license = "";
         level[] = { "", "", -1, "" };
         items[] = {
-            { "arifle_sdar_F", "Taser Rifle", 20000, 7500 },
-            { "hgun_P07_snds_F", "Stun Pistol", 2000, 650 },
-            { "hgun_P07_F", "", 7500, 1500 },
-            { "HandGrenade_Stone", "Flashbang", 1700, -1 },
+            { "hgun_P07_snds_F", "Stun Pistol", 2000, -1 },
             { "Binocular", "", 150, -1 },
             { "ItemGPS", "", 100, 45 },
             { "FirstAidKit", "", 150, 65 },
@@ -156,11 +205,12 @@ class WeaponShops {
     };
 
     class cop_patrol {
-        name = "Altis Patrol Officer Shop";
+        name = "TSP Trooper Shop";
         side = "cop";
         license = "";
         level[] = { "life_coplevel", "SCALAR", 2, "You must be a Patrol Officer Rank!" };
         items[] = {
+            { "hgun_P07_snds_F", "Stun Pistol", 2000, -1 },
             { "arifle_MX_F", "", 35000, 7500 },
             { "SMG_02_ACO_F", "", 30000, -1 },
             { "HandGrenade_Stone", "Flashbang", 1700, -1 },
@@ -170,6 +220,7 @@ class WeaponShops {
             { "muzzle_snds_H", "", 2750, -1 }
         };
         mags[] = {
+            { "16Rnd_9x21_Mag", "", 25 },
             { "30Rnd_65x39_caseless_mag", "", 130 },
             { "30Rnd_9x21_Mag", "", 250 }
         };
